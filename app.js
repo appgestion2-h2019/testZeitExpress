6ec69e238a98526e8e62c5c45b493b1f90abdee3
Carine Croteau <croteau.carine@cegepvicto.ca>
2019-05-09 13:01:16 -0800

    ...

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('*', (req, res) => {
-    res.send(200, '<h1>Hello, world!</h1>')
+    res.send(200, '<h1>Helloooo, world!</h1>')
 })
 
 module.exports = app
